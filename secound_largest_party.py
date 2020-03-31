81218df576969b8987436fb23466a90bd02cfadd
Akula Guru Datta <40553773+datta07@users.noreply.github.com>
2020-03-31 21:04:28 +0530

    Update secound_largest_party.py
    added label

--- secound_largest_party.py
+++ secound_largest_party.py
@@ -74,4 +74,5 @@ for i in patches:
 		ax.add_collection(PatchCollection(patches[i], facecolor= color_data[i], edgecolor=color_data[i], linewidths=.1, zorder=2))
 	except Exception:
 		ax.add_collection(PatchCollection(patches[i], facecolor= '#ffffff', edgecolor='k', linewidths=0.1, zorder=2))
+plt.title('2019 Secound Largest Party')
 plt.show()
